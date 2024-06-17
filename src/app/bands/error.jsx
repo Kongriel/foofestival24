455c6b2301fc10031ddced92f4fc59fe7d21b007
Anton <krauseriel@gmail.com>
2024-06-17 17:11:58 +0200

    error

--- src/app/bands/error.jsx
+++ src/app/bands/error.jsx
@@ -1,12 +1,11 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import errorImage from "";
 
 const Error404Page = () => {
   return (
     <div>
-      <Image src={errorImage} alt="Error 404" width={500} height={400} />
+      <Image src="/404.png" alt="Error 404" width={500} height={400} />
       <h1 style={{ textAlign: "center" }}>ERROR 404</h1>
     </div>
   );
